14852c80e8c7a5a6e050f24ff39c0a35f4ef43ff
SashaBenzar <sahabenzar@gmail.com>
2023-12-19 16:21:18 +0200

    configure eslint with prettier and modify all files

--- app/middleware/signUpValidation.middleware.js
+++ app/middleware/signUpValidation.middleware.js
@@ -1,13 +1,13 @@
-import { check } from 'express-validator';
+import { check } from "express-validator";
 
 const signUpValidator = [
-  check('email', "email isn't correct").isEmail(),
-  check('password', "password can't me less than 4 and more than 15 symbol").isLength({
-    min: 4,
-    max: 15,
-  }),
-  check('username', "username can't be empty").notEmpty(),
-  check('picture', 'make sure you have entered the link correctly').optional().isURL(),
+   check("email", "email isn't correct").isEmail(),
+   check("password", "password can't me less than 4 and more than 15 symbol").isLength({
+      min: 4,
+      max: 15,
+   }),
+   check("username", "username can't be empty").notEmpty(),
+   check("picture", "make sure you have entered the link correctly").optional().isURL(),
 ];
 
 export default signUpValidator;
--- app/routes/User.routes.js
+++ app/routes/User.routes.js
@@ -1,13 +1,14 @@
-import { Router } from 'express';
-import UserController from '../controllers/User.controller.js';
-import signUpValidator from '../middleware/signUpValidation.middleware.js';
-import signInMiddleware from '../middleware/signIn.middleware.js';
-import roleMiddleware from '../middleware/role.middleware.js';
+import { Router } from "express";
+import UserController from "../controllers/User.controller.js";
+import signUpValidator from "../middleware/signUpValidation.middleware.js";
+import roleMiddleware from "../middleware/role.middleware.js";
 
 const userRouter = new Router();
+var a = 5;
 
-userRouter.get('/users', roleMiddleware('admin'), UserController.getAll);
-userRouter.post('/signup', signUpValidator, UserController.signUp);
-userRouter.post('/signin', UserController.signIn);
+console.log(a);
+userRouter.get("/users", roleMiddleware("admin"), UserController.getAll);
+userRouter.post("/signup", signUpValidator, UserController.signUp);
+userRouter.post("/signin", UserController.signIn);
 
 export default userRouter;
