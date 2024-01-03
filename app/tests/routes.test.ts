98decb15642606c9c655465d837dca3805f095e6
SashaBenzar <sahabenzar@gmail.com>
2024-01-03 14:52:53 +0200

    add describe for tests and cover all tests

--- app/tests/routes.test.ts
+++ app/tests/routes.test.ts
@@ -3,8 +3,8 @@ import { app } from "../server";
 import User from "../models/User.model";
 
 describe("routes tests", () => {
-   describe("get all users", () => {
-      it("success", async () => {
+   describe("tested if admin can get all users in database", () => {
+      it("if it is admin then it'll return status code 200", async () => {
          const { body } = await supertest(app).post("/api/signin").send({
             username: "Alex",
             password: "12345",
@@ -14,39 +14,53 @@ describe("routes tests", () => {
             .set("Authorization", "Baerer " + body.token);
          expect(response.statusCode).toBe(200);
       });
-      it("get error", async () => {
+      it("if it is user then it'll return status code 403", async () => {
+         const { body } = await supertest(app).post("/api/signin").send({
+            username: "benzaroo",
+            password: "12345",
+         });
+         const response = await supertest(app)
+            .get("/api/users")
+            .set("Authorization", "Baerer " + body.token);
+         expect(response.statusCode).toBe(403);
+      });
+      it("if it is unregistered user then it'll return status code 403", async () => {
          const response = await supertest(app).get("/api/users");
          expect(response.statusCode).toBe(403);
       });
    });
-   describe("post for sign in", () => {
-      it("success", async () => {
-         const response = await supertest(app).post("/api/signin").send({
-            username: "Alex1",
-            password: "123456",
-         });
-         expect(response.statusCode).toBe(400);
-      });
-      it("error", async () => {
+   describe("tested all valid and invalid cases for sign in", () => {
+      it("if all data is correct then it'll return status code 200", async () => {
          const response = await supertest(app).post("/api/signin").send({
             username: "Alex",
             password: "12345",
          });
          expect(response.statusCode).toBe(200);
       });
-   });
-   describe("post for sign up", () => {
-      it("success", async () => {
-         const response = await supertest(app).post("/api/signup").send({
-            username: "Alex",
-            password: "12345",
-            email: "sahabenzar@gmail.com",
-            isAdmin: false,
-            picture: null,
+      it("if user didn't exist then it'll return status code 400", async () => {
+         const response = await supertest(app).post("/api/signin").send({
+            username: "Alex1",
+            password: "123456",
          });
          expect(response.statusCode).toBe(400);
       });
-      it("error", async () => {
+      it("if data isn't correct then it'll return status code 400", async () => {
+         const response = await supertest(app).post("/api/signin").send({
+            username: "",
+            password: "",
+         });
+         expect(response.statusCode).toBe(400);
+      });
+      it("if user entered wrong password then it'll return status code 400 ", async () => {
+         const response = await supertest(app).post("/api/signin").send({
+            username: "Alex",
+            password: "1234",
+         });
+         expect(response.statusCode).toBe(400);
+      });
+   });
+   describe("tested all valid and invalid cases for sign up", () => {
+      it("if all data is correct then it'll return status code 200", async () => {
          const response = await supertest(app).post("/api/signup").send({
             username: "User1",
             password: "12345",
@@ -59,6 +73,26 @@ describe("routes tests", () => {
          if (user?._id) {
             await User.findByIdAndDelete(user._id);
          }
+      });
+      it("if data isn't correct then it'll return status code 400", async () => {
+         const response = await supertest(app).post("/api/signup").send({
+            username: "",
+            password: "",
+            email: "",
+            isAdmin: false,
+            picture: null,
+         });
+         expect(response.statusCode).toBe(400);
+      });
+      it("if user is already exist then it'll return status code 400", async () => {
+         const response = await supertest(app).post("/api/signup").send({
+            username: "Alex",
+            password: "12345",
+            email: "sahabenzar@gmail.com",
+            isAdmin: false,
+            picture: null,
+         });
+         expect(response.statusCode).toBe(400);
       });
    });
 });
