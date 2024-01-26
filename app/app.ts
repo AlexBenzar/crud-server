7ebdfca99d16767d9961ee37d6f007959fe11f96
AlexBenzar <154214586+AlexBenzar@users.noreply.github.com>
2024-01-26 12:46:21 +0300

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -4,7 +4,7 @@ import userRouter from "./routes/User.routes";
 import cors from "cors";
 import profileRouter from "./routes/Profile.routes";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const DB__URL = `mongodb+srv://olexandrbenzarsifex:12345@crud-database.xlbnxut.mongodb.net/`;
 
 export const app = express();
